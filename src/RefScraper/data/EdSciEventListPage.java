f1daac29ec3904a890a5295a6479188b784e0de3
Alan L <alanlesli@gmail.com>
2012-03-01 20:08:26 +0000

    added utf to asci conversion for '-'

--- src/RefScraper/data/EdSciEventListPage.java
+++ src/RefScraper/data/EdSciEventListPage.java
@@ -150,4 +150,45 @@ public class EdSciEventListPage {
             Logger.getLogger(EdSciEventListPage.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    static String getAsciiText(String theText) {
+        StringBuilder theBuilder = new StringBuilder();
+        int lengthInChars = theText.length();
+        int noOfCodePoints = theText.codePointCount(0, lengthInChars - 1);
+
+        try {
+            if (lengthInChars > 0
+                    && lengthInChars > noOfCodePoints) {
+                for (int offset = 0; offset < lengthInChars;) {
+                    final int codePoint = theText.codePointAt(offset);
+                    char theCharAt = theText.charAt(offset);
+
+                    if (codePoint >= 0 && codePoint < 128) {
+                        theBuilder.append(theCharAt);
+                    } else {
+                        theBuilder.append(EdSciEventListPage.asciiFromUTF(codePoint));
+                    }
+
+                    offset += Character.charCount(codePoint);
+                }
+            } else {
+                theBuilder.append(theText);
+            }
+        } catch (IndexOutOfBoundsException exc) {
+            System.out.println("out of bounds");
+        }
+
+        return theBuilder.toString();
+    }
+    
+    static char asciiFromUTF(int codePoint) {
+        char retVal = ' ';
+        switch (codePoint) {
+            case 8211:
+                retVal = '-';
+                break;
+        }
+
+        return retVal;
+    }
 }
